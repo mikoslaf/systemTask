f3be19bd68e5fc4794ba46e1a61a17bfeb5e3160
mikoslaf <mikolaj2003wojcik@gmail.com>
2024-10-26 09:45:05 +0200

    add buttons in taskitem component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,10 @@ import { Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { AddTaskComponent } from './add-task/add-task.component';
 import { TasksComponent } from './tasks/tasks.component';
-import { TaskitemComponent } from './taskitem/taskitem.component';
 
 export const routes: Routes = [
     { path: "", component: MainComponent },
     { path: "add", component: AddTaskComponent },
     { path: "tasks", component: TasksComponent },
-    { path: "taskitem", component: TaskitemComponent}
+    { path: "add/:id", component: AddTaskComponent}
 ];
--- src/app/taskitem/taskitem.component.ts
+++ src/app/taskitem/taskitem.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Task } from '../task';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-taskitem',
@@ -10,4 +11,13 @@ import { Task } from '../task';
 })
 export class TaskitemComponent {
   @Input() dane:Task = {id: -1, active: false, name: "", status: 0, taskEnd: new Date(), taskStart: new Date(), work:[]};
+  constructor(private router: Router){}
+
+  public edit(id: number) {
+    this.router.navigate(['/add', id]);
+  }
+
+  public remove(id: number) {
+    // add in the futhere
+  }
 }
